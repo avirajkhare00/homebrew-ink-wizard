d5593ebeba084a554a5dfd8e526d37defb015a96
Aviraj Khare <thisisavirajkhare@gmail.com>
2023-01-15 18:24:46 +0530

    [Aviraj] Initial commit

---
+++ Formula/ink-wizard.rb
@@ -0,0 +1,40 @@
+class InkWizard < Formula
+  include Language::Python::Virtualenv
+  desc "CLI tool to generate Flipper, PSP22, PSP34, PSP37 smart contracts in Ink."
+  homepage "https://github.com/avirajkhare00/ink-wizard"
+  url "https://files.pythonhosted.org/packages/86/26/caea7d97eee75851f68c84c6470f3095365410ce855714c0e08d729960fb/ink-wizard-0.1.4.tar.gz"
+  sha256 "0cd6f31dfabe5e4cac132520f5890ca486c278a5891634506ae1202aaddc205f"
+  license "Apache-2.0"
+
+  depends_on "python@3.8"
+
+  resource "Jinja2" do
+    url "https://files.pythonhosted.org/packages/7a/ff/75c28576a1d900e87eb6335b063fab47a8ef3c8b4d88524c4bf78f670cce/Jinja2-3.1.2.tar.gz"
+    sha256 "31351a702a408a9e7595a8fc6150fc3f43bb6bf7e319770cbc0db9df9437e852"
+  end
+
+  resource "typer" do
+    url "https://files.pythonhosted.org/packages/e1/45/bcbc581f87c8d8f2a56b513eb994d07ea4546322818d95dc6a3caf2c928b/typer-0.7.0.tar.gz"
+    sha256 "ff797846578a9f2a201b53442aedeb543319466870fbe1c701eab66dd7681165"
+  end
+
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/59/87/84326af34517fca8c58418d148f2403df25303e02736832403587318e9e8/click-8.1.3.tar.gz"
+    sha256 "7682dc8afb30297001674575ea00d1814d808d6a36af415a82bd481d37ba7b8e"
+  end
+
+  resource "MarkupSafe" do
+    url "https://files.pythonhosted.org/packages/1d/97/2288fe498044284f39ab8950703e88abbac2abbdf65524d576157af70556/MarkupSafe-2.1.1.tar.gz"
+    sha256 "7f91197cc9e48f989d12e4e6fbc46495c446636dfc81b9ccf50bb0ec74b91d4b"
+  end
+
+  resource "tomli" do
+    url "https://files.pythonhosted.org/packages/c0/3f/d7af728f075fb08564c5949a9c95e44352e23dee646869fa104a3b2060a3/tomli-2.0.1.tar.gz"
+    sha256 "de526c12914f0c550d15924c62d72abc48d6fe7364aa87328337a31007fe8a4f"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+end
